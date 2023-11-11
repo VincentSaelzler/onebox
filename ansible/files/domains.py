535ebe1a2eba9abc7bd364487f7711a4d967f080
Vincent Saelzler <vincent.saelzler@gmail.com>
2023-11-11 18:30:50 +0000

    refactored and running on dev container

--- ansible/files/domains.py
+++ ansible/files/domains.py
@@ -1,29 +1,59 @@
 import requests
 import json
 
-BASE_URL = "https://api.godaddy.com"
+import subprocess
 
-# these are just for testing
-API_KEY = "dKst8pghnmJ4_Wh8njbkCKACzqVScuyUDvE"
-API_SECRET = "UUjLvyCJU8tdp84sV4kMNf"
-
-endpoint = "/v1/domains"
-
+# hard coded assumptions
+DOMAINS = ["hearty.cooking"]
+BASE_URL = "https://api.godaddy.com/v1"
+API_KEY = "dKst8pghnmJ4_Wh8njbkCKACzqVScuyUDvE"  # CHANGE
+API_SECRET = "UUjLvyCJU8tdp84sV4kMNf"  # CHANGE
 headers = {
-    'Authorization': f'sso-key {API_KEY}:{API_SECRET}',
-    'Content-Type': 'application/json'
+    "Authorization": f"sso-key {API_KEY}:{API_SECRET}",
+    "Content-Type": "application/json",
 }
 
-r = requests.get(f'{BASE_URL}{endpoint}/hearty.cooking/records',headers=headers)
 
-records = json.loads(r.text)
-root_host_records = [record for record in records if record['type'] == 'A' and record['name'] == '@']
-
-if len(root_host_records) != 1:
-    raise IndexError('expecting exactly one host record for the domain root')
-
-root_host_record = root_host_records[0]
-
-print(json.dumps(o, indent=2))
+def get_public_ip():
+    dig_output = subprocess.run(
+        ["dig", "+short", "myip.opendns.com", "@resolver1.opendns.com"],
+        capture_output=True,
+        text=True,
+    )
+    return str.strip(dig_output.stdout)
 
 
+def get_root_host_record(root_host_record_endpoint):
+    # https://developer.godaddy.com/doc/endpoint/domains#/v1/recordGet
+    get_response = requests.get(root_host_record_endpoint, headers=headers)
+    root_host_records = json.loads(get_response.text)
+
+    if len(root_host_records) != 1:
+        raise IndexError("expecting exactly one host record for the domain root")
+
+    return root_host_records[0]
+
+
+def put_root_host_record(root_host_record_endpoint, root_host_record):
+    # returns a blank 200 response whether or not changes occur
+    # https://developer.godaddy.com/doc/endpoint/domains#/v1/recordReplaceTypeName
+    root_host_records = json.dumps([root_host_record])  # api requires an array
+    put_response = requests.put(
+        root_host_record_endpoint,
+        data=root_host_records,
+        headers=headers,
+    )
+
+
+for domain in DOMAINS:
+    root_host_record_endpoint = f"{BASE_URL}/domains/{domain}/records/A/@"
+
+    public_ip = get_public_ip()
+    root_host_record = get_root_host_record(root_host_record_endpoint)
+
+    if root_host_record["data"] != public_ip:
+        root_host_record["data"] = public_ip
+        put_root_host_record(root_host_record_endpoint, root_host_record)
+
+
+print("made it!")
